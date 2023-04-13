4db8810c9254a6ec2c43fb6fa87c1bf66204f210
vijit kumar <vijitkumar699@gmail.com>
2023-04-13 16:48:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let trans = document.getElementById("trans")
+let trans2 = document.getElementById("trans2")
+trans2.addEventListener("click",()=>{
+    trans.classList.toggle("translate-x-[-100%]")
+})
